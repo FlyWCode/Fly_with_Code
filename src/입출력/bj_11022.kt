851f03ab18ba5f7b287f78732da4086b1605859b
KyungMin <2004ckck@naver.com>
2021-04-20 23:02:19 +0900

    [CREATE] A+b 백준 11022

---
+++ src/입출력/bj_11022.kt
@@ -0,0 +1,14 @@
+package 입출력
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+
+    val x=nextInt()
+    for(i in 1..x){
+        var a=nextInt()
+        var b=nextInt()
+        print("Case #$i: $a + $b = ${a+b} ")
+    }
+
+}
