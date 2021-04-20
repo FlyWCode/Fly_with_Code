8ff514c24c08b7b51c43a4f2d40ff8222433cab1
KyungMin <2004ckck@naver.com>
2021-04-20 22:33:33 +0900

    [CREATE] A+b 백준 10953

--- src/입출력/bj_10953.kt
+++ src/입출력/bj_10953.kt
@@ -3,13 +3,13 @@ package 입출력
 import java.util.*
 
 
-fun main() = with(Scanner(System.`in`)){
-    val x=nextInt()
-    for (i in 1..x){
-        val a=next()
+fun main() = with(Scanner(System.`in`)) {
 
-        val array=a.split(",".toRegex())
-        println(array[0].toInt()+array[1].toInt())
+    for (i in 1..nextInt()) {
+        val a = next()
+
+        val array = a.split(",".toRegex())
+        println(array[0].toInt() + array[1].toInt())
     }
 
 }
