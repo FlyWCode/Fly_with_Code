0f9f52fa6b02ae6c9271623ce846b864871d0911
KyungMin <2004ckck@naver.com>
2021-07-05 16:53:10 +0900

    [ADD] 별찍기-1

---
+++ ChaKyungMin/백준/bj_2438.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val number= readLine()!!.toInt()
+
+    for (i in 1..number){
+        // repeat 특정 문자열 or 함수 만큼 반복 i 만큼 반복이니 i 가 1 일때 * 2일때 ** 등등.. 나간다
+       print("*".repeat(i))
+        print("\n")
+    }
+}
